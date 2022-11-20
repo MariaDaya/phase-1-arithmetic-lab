40285c76373230f08d4771d8fd327ce4a959a8c9
Maria Dayekh <mariaaldayekh@gmail.com>
2022-11-20 12:18:13 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2= 2
+var multiply = num1 * num2
+
+var random =  Math.round( Math.random() *10) + 1
+console.log(random)
+
+let num3= 18
+let num4 =7
+var mod = num3 % num4
+console.log(mod)
+
+var max= Math.max( 15, 20, 18, 6)
